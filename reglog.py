06a7c84a953eddfc38673b933c0015f84657628b
ManonLefevre <you@example.com>
2022-03-17 19:22:44 +0100

    maj

--- reglog.py
+++ reglog.py
@@ -6,9 +6,24 @@ class MSELoss(Loss):
     def __init__(self):
         super().__init__()
     def forward(self, y, yhat):
+        """ Calcul du coût aux moindres carrés (mse).
+            input : y est de taille batch*d
+                    yhat est de taille batch*d
+            output : res est de taille batch
+        """
         assert y.shape==yhat.shape
-        res = np.linalg.norm(y[i]-yhat[i],2)**2
+        res = np.linalg.norm( y-yhat, axis=1) ** 2
         assert len(res)==y.shape[0]
+        return res
+    def backward(self, y, yhat):
+        """ Calcule le gradient du coût aux moindres carrés.
+                input : y est de taille batch*d
+                        yhat est de taille batch*d
+                output : res est de taille batch*d
+        """
+        assert y.shape == yhat.shape
+        res = 2 * (y-yhat)
+        assert res.shape == y.shape
         return res
 
 class Linear(Module):
@@ -17,12 +32,13 @@ class Linear(Module):
         self.input=input
         self.output=output
     def forward(self, X):
-        ## Calcule la passe forward
-        #calculer les sorties du module pour les entrées passées en paramètre
+        """ Calcule la passe forward
+                input : X est de taille batch*input
+                output : res est de taille batch*output
+        """
         assert X.shape[1]==len(self.input)
-        res=[]
-        m = MSELoss()
         assert res.shape[1]==len(self.out)
+        assert X.shape[0]==res.shape[0]
         return res
 
 
@@ -31,3 +47,4 @@ y=np.ones((2,6))
 yhat=np.ones((2,6))
 yhat[1]=0
 print(m.forward(y,yhat))
+print(m.backward(y,yhat))
