85cda23c0e0e435779d67584bf5c0226e585bc37
ManonLefevre <you@example.com>
2022-04-17 20:08:03 +0200

    main linear avec mltools update

--- main_mltools.py
+++ main_mltools.py
@@ -3,7 +3,7 @@ from linear import Linear
 from mse import MSELoss
 import matplotlib.pyplot as plt
 
-#TO DO : loss croissante 
+#TO DO : loss croissante
 
 datax, datay = gen_arti(data_type=0,epsilon=0.1,nbex=1000)
 
@@ -22,9 +22,6 @@ for i in range(100):
     lin.update_parameters(gradient_step=1e-2)
     all_w.append(lin._parameters.copy())
 w_star=lin._parameters.copy()
-
-## Fabrication d'une grille de discrétisation pour la visualisation de la fonction de coût
-grid, x_grid, y_grid = make_grid(xmin=-2, xmax=2, ymin=-2, ymax=2, step=100)
 
 ## Visualisation des données et de la frontière de décision
 plt.figure()
